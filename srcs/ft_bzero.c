ff1b7ced3e46a9e6b9d83b6a2a98f10036676a10
acourtin <acourtin@student.42.fr>
2017-11-07 12:02:04 +0100

    ajout de bzero

--- srcs/ft_bzero.c
+++ srcs/ft_bzero.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/07 11:14:59 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/07 11:46:12 by acourtin         ###   ########.fr       */
+/*   Created: 2017/11/07 11:43:56 by acourtin          #+#    #+#             */
+/*   Updated: 2017/11/07 12:01:46 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n)
+{
+	int				i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = s;
+	while (i < n)
+	{
+		*ptr = '\0';
+		ptr++;
+		i++;
+	}
+}
