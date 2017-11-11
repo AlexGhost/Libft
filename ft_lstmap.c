75f16042bc4d4251f6e2744ea16203d8e8291663
acourtin <acourtin@student.42.fr>
2017-11-11 20:20:35 +0100

    ajout final

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/11 17:02:59 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/11 19:01:29 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/11 20:19:41 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list		*newlist;
 	t_list		*curlist;
+	t_list		**a;
 
-	newlist = (t_list*)malloc(sizeof(t_list));
+	curlist = lst;
+	newlist = ft_lstnew(f(curlist)->content, f(curlist)->content_size);
 	if (!newlist)
 		return (NULL);
-	curlist = lst;
+	a = &newlist;
 	while (curlist->next != NULL)
 	{
-		ft_lsttail(&newlist, ft_lstnew(f(curlist)->content, f(curlist)->content_size));
 		curlist = curlist->next;
+		ft_lsttail(a, ft_lstnew(f(curlist)->content, f(curlist)->content_size));
 	}
-	ft_lsttail(&newlist, ft_lstnew(f(curlist)->content, f(curlist)->content_size));
+	free(newlist);
 	return (newlist);
 }
