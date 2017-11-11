dd6fa0322a792caf43d95ba35fdd8258c6a1c94b
acourtin <acourtin@student.42.fr>
2017-11-11 22:24:09 +0100

    bon c bon

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 16:00:32 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/11 22:08:09 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/11 22:21:48 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 		i = 0;
 		taille1 = 0;
 		taille2 = 0;
-		while (s1[taille1] != '\0' && i < taille1)
+		while (s1[taille1] != '\0' && taille1 < n)
 			taille1++;
-		while (s2[taille2] != '\0' && i < taille2)
+		while (s2[taille2] != '\0' && taille2 < n)
 			taille2++;
 		if (taille1 != taille2)
 			return (0);
